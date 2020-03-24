434c540394426e447643b8b1cdef60bfe2d07309
“luola” <ll63702168@qq.com>
2020-03-24 21:06:36 +0800

    up

--- MxOnline/apps/utils/mixin_utils.py
+++ MxOnline/apps/utils/mixin_utils.py
@@ -8,3 +8,4 @@ class LoginRequiredMixin(object):
     @method_decorator(login_required(login_url='/users/login/'))
     def dispatch(self, request, *args, **kwargs):
         return super(LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
+
